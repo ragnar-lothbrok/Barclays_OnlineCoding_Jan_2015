0acc1e5d27367701b1a3369661564a32820dc578
Raghunandan <raghunitb@gmail.com>
2015-01-16 15:13:17 +0530

    Update Solution2_longest_increasing_sequence_diff_1.java
    Issue is fixed.

--- src/com/test/barclays/Solution2_longest_increasing_sequence_diff_1.java
+++ src/com/test/barclays/Solution2_longest_increasing_sequence_diff_1.java
@@ -4,27 +4,41 @@ import java.util.Arrays;
 public class Solution2_longest_increasing_sequence_diff_1 {
 
     public int solution(int[] A) {
-        int min = 0;
-        int prevlength = 0;
-        int currLength = 0;
+        int length = 0;
         Arrays.sort(A);
-        for (int i = 0; i < A.length; i++) {
-            if (currLength == 0)
-                min = A[i];
-            if (A[i] - min < 2) {
-                currLength++;
-            } else {
-                min = A[i];
-                if (prevlength < currLength) {
-                    prevlength = currLength;
-                }
-                currLength = 0;
-            }
-            if (i == A.length - 1 && currLength > prevlength) {
-                prevlength = currLength;
+        String uniqueValueIndexes = null;
+        if (A.length == 1)
+            return 1;
+        uniqueValueIndexes = "0";
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] != A[i - 1]) {
+                uniqueValueIndexes = uniqueValueIndexes + "," + i + "";
+                if (i == A.length - 2)
+                    uniqueValueIndexes = uniqueValueIndexes + "," + i + "";
             }
         }
-        return prevlength;
+
+        String[] tokens = uniqueValueIndexes.split(",");
+        if (tokens.length == 1)
+            length = tokens.length;
+
+        System.out.println(uniqueValueIndexes);
+
+        for (int i = 0; i < tokens.length - 1; i++) {
+            int temp = 0;
+            if (A[Integer.parseInt(tokens[i + 1])] - A[Integer.parseInt(tokens[i])] < 2) {
+                if (tokens.length > i + 2) {
+                    temp = Integer.parseInt(tokens[i + 2]) - Integer.parseInt(tokens[i]);
+                } else {
+                    temp = A.length - Integer.parseInt(tokens[i]);
+                }
+            } else {
+                temp = Integer.parseInt(tokens[i + 1]) - Integer.parseInt(tokens[i]);
+            }
+            if (length < temp)
+                length = temp;
+        }
+        return length;
     }
 
 }
