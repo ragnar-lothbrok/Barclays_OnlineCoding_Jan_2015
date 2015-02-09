b260f1fd7f01272c498707eb8b6a4501362f756f
Raghunandan <raghunitb@gmail.com>
2015-02-09 10:56:52 +0530

    alternate method added

--- src/com/test/barclays/Solution1_1swap_inreasing_order_array.java
+++ src/com/test/barclays/Solution1_1swap_inreasing_order_array.java
@@ -25,4 +25,26 @@ public class Solution1_1swap_inreasing_order_array {
         return result;
     }
     
+    public boolean solution1(int[] A) {
+        int temp = 0, swapCount = 0;
+        boolean result = true;
+        for (int i = 0; i < A.length - 1; i++) {
+            if (A[i] <= A[i + 1]) {
+                continue;
+            } else {
+                if (swapCount == 1) {
+                    result = false;
+                    break;
+                } else {
+                    temp = A[i];
+                    A[i] = A[i + 1];
+                    A[i + 1] = temp;
+                    swapCount++;
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+    
 }
